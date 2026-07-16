f78d1b89cc08a2a5e919d0393e746e041a9afded
OktavianusKimmy <kimmyyonata24@gmail.com>
2026-07-16 09:01:17 +0700

    adding route to Mulai belajar button on header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,9 +41,17 @@
 
         </div>
 
-        <button class="bg-yellow-400 text-black text-sm font-semibold px-4 py-2 rounded-lg">
-            Mulai belajar
-        </button>
+        @guest
+            <a href="{{ route('auth.redirect') }}" class="bg-yellow-400 text-black text-sm font-semibold px-4 py-2 rounded-lg">
+                Mulai belajar
+            </a>
+        @endguest
+
+        @auth
+            <a href="{{ route('study-room') }}" class="bg-yellow-400 text-black text-sm font-semibold px-4 py-2 rounded-lg">
+                Mulai belajar
+            </a>
+        @endauth
     </div>
 </section>
 
